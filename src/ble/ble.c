59c0c85ba2c7d1177805d8c5a3397cbc2d082c1c
Sean Kelly <theseankelly@outlook.com>
2016-01-31 15:38:40 -0800

    Implementation of device manager in the BLE code to support pairing and authentication (required for use with Windows BLE hosts)

--- src/ble/ble.c
+++ src/ble/ble.c
@@ -31,6 +31,8 @@
 #include "softdevice_handler.h"
 #include "nrf_gpio.h"
 #include "ble_debug_assert_handler.h"
+#include "pstorage.h"
+#include "device_manager.h"
 
 #include "timeslot.h"
 
@@ -76,6 +78,8 @@
 
 static ble_gap_sec_params_t                  m_sec_params;                             /**< Security requirements for this application. */
 static ble_gap_adv_params_t                  m_adv_params;                             /**< Parameters to be passed to the stack when starting advertising. */
+
+static dm_application_instance_t             m_app_handle;                             /**< Application identifier allocated by device manager */
 
 static void ble_evt_dispatch(ble_evt_t * p_ble_evt);
 
@@ -155,7 +159,51 @@ static void bond_manager_error_handler(uint32_t nrf_error)
 * Static Initialization Functions
 *****************************************************************************/
 
+/**@brief Function for handling the Device Manager events.
+ *
+ * @param[in]   p_evt   Data associated to the device manager event.
+ */
+static uint32_t device_manager_evt_handler(dm_handle_t const    * p_handle,
+                                           dm_event_t const     * p_event,
+                                           api_result_t           event_result)
+{
+    APP_ERROR_CHECK(event_result);
+    return NRF_SUCCESS;
+}
 
+/**@brief Function for the Device Manager initialization.
+ */
+static void device_manager_init(void)
+{
+    uint32_t                err_code;
+    dm_init_param_t         init_data;
+    dm_application_param_t  register_param;
+
+    // Initialize persistent storage module.
+    err_code = pstorage_init();
+    APP_ERROR_CHECK(err_code);
+
+    // Clear all bonded centrals if the Bonds Delete button is pushed.
+    init_data.clear_persistent_data = false;
+
+    err_code = dm_init(&init_data);
+    APP_ERROR_CHECK(err_code);
+
+    memset(&register_param.sec_param, 0, sizeof(ble_gap_sec_params_t));
+
+    register_param.sec_param.timeout      = SEC_PARAM_TIMEOUT;
+    register_param.sec_param.bond         = SEC_PARAM_BOND;
+    register_param.sec_param.mitm         = SEC_PARAM_MITM;
+    register_param.sec_param.io_caps      = SEC_PARAM_IO_CAPABILITIES;
+    register_param.sec_param.oob          = SEC_PARAM_OOB;
+    register_param.sec_param.min_key_size = SEC_PARAM_MIN_KEY_SIZE;
+    register_param.sec_param.max_key_size = SEC_PARAM_MAX_KEY_SIZE;
+    register_param.evt_handler            = device_manager_evt_handler;
+    register_param.service_type           = DM_PROTOCOL_CNTXT_GATT_SRVR_ID;
+
+    err_code = dm_register(&m_app_handle, &register_param);
+    APP_ERROR_CHECK(err_code);
+}
 
 /**@brief Function for the GAP initialization.
  *
@@ -291,7 +339,7 @@ static void conn_params_init(void)
     cp_init.first_conn_params_update_delay = FIRST_CONN_PARAMS_UPDATE_DELAY;
     cp_init.next_conn_params_update_delay  = NEXT_CONN_PARAMS_UPDATE_DELAY;
     cp_init.max_conn_params_update_count   = MAX_CONN_PARAMS_UPDATE_COUNT;
-    //cp_init.start_on_notify_cccd_handle    = m_hrs.hrm_handles.cccd_handle;
+    cp_init.start_on_notify_cccd_handle    = BLE_GATT_HANDLE_INVALID;
     cp_init.disconnect_on_fail             = true;
     cp_init.evt_handler                    = NULL;
     cp_init.error_handler                  = conn_params_error_handler;
@@ -363,27 +411,14 @@ void ble_advertising_stop(void)
  */
 static void on_ble_evt(ble_evt_t * p_ble_evt)
 {
-    uint32_t        err_code;
-    static uint16_t m_conn_handle = BLE_CONN_HANDLE_INVALID;
-
     switch (p_ble_evt->header.evt_id)
     {
         case BLE_GAP_EVT_CONNECTED:
-
-            m_conn_handle = p_ble_evt->evt.gap_evt.conn_handle;
-
             //APP_ERROR_CHECK(err_code);
             break;
 
         case BLE_GAP_EVT_DISCONNECTED:
             advertising_start();
-            break;
-
-        case BLE_GAP_EVT_SEC_PARAMS_REQUEST:
-            err_code = sd_ble_gap_sec_params_reply(m_conn_handle,
-                                                   BLE_GAP_SEC_STATUS_SUCCESS,
-                                                   &m_sec_params);
-            APP_ERROR_CHECK(err_code);
             break;
 
         case BLE_GAP_EVT_TIMEOUT:
@@ -409,8 +444,9 @@ static void on_ble_evt(ble_evt_t * p_ble_evt)
  */
 static void ble_evt_dispatch(ble_evt_t * p_ble_evt)
 {
-    ble_crazyflies_on_ble_evt(p_ble_evt);
+    dm_ble_evt_handler(p_ble_evt);
     ble_conn_params_on_ble_evt(p_ble_evt);
+    ble_crazyflies_on_ble_evt(p_ble_evt);
     on_ble_evt(p_ble_evt);
 }
 
@@ -424,6 +460,7 @@ static void ble_evt_dispatch(ble_evt_t * p_ble_evt)
  */
 static void sys_evt_dispatch(uint32_t sys_evt)
 {
+  pstorage_sys_event_handler(sys_evt);
   timeslot_sd_evt_signal(sys_evt);
 }
 
@@ -442,6 +479,7 @@ int ble_init(void)
     ble_stack_init();
 
     // Initialize Bluetooth Stack parameters
+    device_manager_init();
     gap_params_init();
     advertising_init();
     services_init();
