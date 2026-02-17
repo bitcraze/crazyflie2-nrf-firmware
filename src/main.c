87a20a4bf5feb2eabd9b72af23a64b6dbb32b9aa
Rik Bouwmeester <rikjbouwmeester@gmail.com>
2026-02-17 11:20:55 +0100

    Add missing break in SYSLINK_RADIO_READY case, introduced by merge of deckctrl-dfu

--- src/main.c
+++ src/main.c
@@ -458,6 +458,7 @@ static void handleSyslinkEvents(bool slReceived)
         slTxPacket.type = SYSLINK_RADIO_READY;
         slTxPacket.length = 0;
         syslinkSend(&slTxPacket);
+        break;
       case SYSLINK_PM_DECKCTRL_DFU:
         pmDeckctrlDfu(slRxPacket.data[0]);
         break;
